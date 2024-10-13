f6ea685cd21e371cd31932d977244d2ba63ca423
Luiz E <doginuwu@gmail.com>
2024-10-13 10:39:43 -0300

    feat: add global offset of boot sector

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,14 +1,12 @@
+[org 0x7c00]
+
 start:
 	mov ah, 0x0e ; https://en.wikipedia.org/wiki/BIOS_interrupt_call
-	mov al, 'H'
-	int 0x10 ; call 10h - BIOS video service
-	mov al, 'e'
+	mov al, [the_secret]
 	int 0x10
-	mov al, 'l'
-	int 0x10
-	int 0x10
-	mov al, 'o'
-	int 0x10
+
+the_secret:
+	db "X"
 
 times 510-($-$$) db 0
 
