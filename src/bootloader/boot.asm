b13f3a483bfe681280e0a10f21c6a475036794cd
Luiz E <doginuwu@gmail.com>
2024-10-13 10:52:17 -0300

    feat: add stack example

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -2,11 +2,31 @@
 
 start:
 	mov ah, 0x0e ; https://en.wikipedia.org/wiki/BIOS_interrupt_call
-	mov al, [the_secret]
+
+	mov bp, 0x8000 ; base of the stack
+	mov sp, bp ; the stack if empty, the top is the base
+
+	push 'A'
+	push 'B'
+	push 'C'
+
+	mov al, [0x7FFE] ; 0x8000 - 2 - Its only possible acces the stack top
 	int 0x10
 
-the_secret:
-	db "X"
+	pop bx ; C
+	mov al, bl ; Move the lower BX to al
+	int 0x10
+
+	pop bx ; B
+	mov al, bl 
+	int 0x10
+
+	pop bx ; A
+	mov al, bl
+	int 0x10
+
+	mov al, [0x8000] ; garbage
+	int 0x10
 
 times 510-($-$$) db 0
 
