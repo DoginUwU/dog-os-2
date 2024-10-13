c26f0ef48de9bdc389c55bbdb5f591e881bb72c6
Luiz E <doginuwu@gmail.com>
2024-10-13 20:54:35 -0300

    feat: scroll view when needed

--- src/drivers/screen.c
+++ src/drivers/screen.c
@@ -1,4 +1,5 @@
 #include "screen.h"
+#include "../kernel/util.h"
 #include "ports.h"
 
 int get_cursor_offset();
@@ -66,6 +67,23 @@ int print_char(char c, int col, int row, char attr) {
     video_memory[offset + 1] = attr;
 
     offset += 2;
+  }
+
+  if (offset >= MAX_ROWS * MAX_COLS * 2) {
+    int i;
+
+    for (i = 1; i < MAX_ROWS; i++) {
+      memory_copy((char *)get_offset(0, i) + VIDEO_ADDRESS,
+                  (char *)get_offset(0, i - 1) + VIDEO_ADDRESS, MAX_COLS * 2);
+    }
+
+    char *last_line = (char *)get_offset(0, MAX_ROWS - 1) + VIDEO_ADDRESS;
+
+    for (i = 0; i < MAX_COLS * 2; i++) {
+      last_line[i] = 0;
+    }
+
+    offset -= 2 * MAX_COLS;
   }
 
   set_cursor_offset(offset);
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,8 +1,14 @@
 #include "../drivers/screen.h"
+#include "util.h"
 
 void main() {
   clear_screen();
 
-  print("Primeira linha\n");
-  print("outro print");
+  int i = 0;
+
+  for (i = 0; i < 24; i++) {
+    char str[255];
+    int_to_ascii(i, str);
+    print_at(str, 0, i);
+  }
 }
