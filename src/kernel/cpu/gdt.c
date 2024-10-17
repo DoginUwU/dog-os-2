195dc17367d9edc09b9764509218309519916491
Luiz E <doginuwu@gmail.com>
2024-10-17 17:14:57 -0300

    feat: add gdt

---
+++ src/kernel/cpu/gdt.c
@@ -0,0 +1,32 @@
+#include <cpu/gdt.h>
+
+extern void gdt_flush(gdt_ptr_t *);
+
+gdt_entry_t gdt_entries[5];
+gdt_ptr_t gdt_ptr;
+
+void init_gdt() {
+  gdt_ptr.limit = (sizeof(struct gdt_entry_struct) * 5) - 1;
+  gdt_ptr.base = (uint32_t)&gdt_entries;
+
+  set_gdt_gate(0, 0, 0, 0, 0);
+  set_gdt_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+  set_gdt_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+  set_gdt_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF);
+  set_gdt_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF);
+
+  gdt_flush(&gdt_ptr);
+}
+
+void set_gdt_gate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access,
+                  uint8_t gran) {
+  gdt_entries[num].base_low = (base & 0xFFFF);
+  gdt_entries[num].base_middle = (base >> 16) & 0xFF;
+  gdt_entries[num].base_high = (base >> 24) & 0xFF;
+
+  gdt_entries[num].limit_low = (limit & 0xFFFF);
+  gdt_entries[num].granularity = (limit >> 16) & 0x0F;
+
+  gdt_entries[num].granularity |= gran & 0xF0;
+  gdt_entries[num].access = access;
+}
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,7 +1,9 @@
-#include <cpu/idt.h>
+#include <cpu/gdt.h>
 #include <drivers/screen.h>
 
 void kernel_main() {
   clear_screen();
   print("Welcome to DogOS\n");
+
+  init_gdt();
 }
