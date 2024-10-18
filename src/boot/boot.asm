4264d218d9a2f7d4783f7bde90b7906eb37a9cd4
Luiz E <doginuwu@gmail.com>
2024-10-18 15:54:57 -0300

    feat: add initial paging and higher half kernel

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,22 +1,70 @@
-[bits 32]
-section .text
-	align 4
+bits 32
 
-[global _start]
-[extern kernel_main]
+PAGE_ALIGN equ 1 << 0
+MEM_INFO equ 1 << 1
+USE_GFX equ 0
 
-_start:
-		cli
-		mov esp, stack_space
-		call kernel_main
-		jmp $
+MAGIC_NUMBER equ 0x1BADB002
+FLAGS equ PAGE_ALIGN | MEM_INFO | USE_GFX
+CHECKSUM equ -(MAGIC_NUMBER + FLAGS)
 
-HaltKernel:
-	cli
-	hlt
-	jmp HaltKernel
+section .multiboot
+align 4
+	dd MAGIC_NUMBER
+	dd FLAGS
+	dd CHECKSUM
+	dd 0, 0, 0, 0, 0
+
+	dd 0   ; Graphic mode
+	dd 800 ; Width
+	dd 600 ; Height
+	dd 32  ; Depth
 
 section .bss
-resb 8192
+align 16
+stack_bottom:
+	resb 16384 * 8
+stack_top:
 
-stack_space:
+section .boot
+global _start
+
+_start:
+	mov eax, (initial_page_dir - 0xC0000000)
+	mov cr3, eax ; page directory loc
+
+	mov ecx, cr4
+	or ecx, 0x10
+	mov cr4, ecx
+
+	mov ecx, cr0
+	or ecx, 0x80000000 ; enable 32-bit paging
+	mov cr0, ecx
+
+	jmp higher_half
+
+section .text
+extern kernel_main
+higher_half:
+	mov esp, stack_top
+	push ebx
+	xor ebp, ebp
+	call kernel_main
+
+halt:
+	hlt
+	jmp halt
+
+section .data
+align 4096
+global initial_page_dir
+
+initial_page_dir:
+	dd 10000011b
+	times 768-1 dd 0
+
+	dd (0 << 22) | 10000011b
+	dd (1 << 22) | 10000011b
+	dd (2 << 22) | 10000011b
+	dd (3 << 22) | 10000011b
+	times 256-4 dd 0
