3e353e5276582bf54fb1af5d40bbda8e719cc9c3
Luiz E <doginuwu@gmail.com>
2024-10-23 08:29:22 -0300

    feat: add initial malloc header

--- src/include/lib/memory/malloc.h
+++ src/include/lib/memory/malloc.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
+void *malloc(const size_t size);
+void free(const void *ptr);
+
 void *kmalloc(size_t size);
 void *kmalloc_align(size_t size);
 void *kmalloc_align_pysical(size_t size, uint32_t *physical_address);
--- src/kernel/lib/memory/malloc.c
+++ src/kernel/lib/memory/malloc.c
@@ -7,6 +7,17 @@
 extern uint32_t kernel_virtual_end;
 uint32_t placement_address = (uint32_t)&kernel_virtual_end;
 
+void *malloc(const size_t size) {
+  void *ptr = 0;
+
+  asm volatile("int $0x80" ::"a"(3), "b"(size));
+  asm volatile("movl %%eax, %0" : "=r"(ptr));
+
+  return ptr;
+}
+
+void free(const void *ptr) { asm volatile("int $0x80" ::"a"(4), "b"(ptr)); }
+
 void *kmalloc_internal(size_t size, int align, uint32_t *physical_address) {
   if (align == 1 && (placement_address & 0xFFFFF000)) {
     placement_address &= 0xFFFFF000;
